0a995bce49653059ae433fe3259a644d8b051530
Amiko Elvis <amikoelvis@gmail.com>
2025-07-29 16:35:52 +0300

    API Development for Listings and Bookings in Django

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
 
-# Create your views here.
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint to view or edit Listings.
+    Provides GET, POST, PUT, DELETE automatically.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint to view or edit Bookings.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
